48326b92b822ce04feb355010a63b1a33a96a523
ddxbugs <ddxbugs@proton.me>
2024-04-16 20:05:21 -0700

    app/app.R update layout ui input

--- app/app.R
+++ app/app.R
@@ -7,73 +7,37 @@
 #    https://shiny.posit.co/
 #
 
-
-
 library(shiny)
 library(bslib)
 library(ggplot2)
-library(maps)
-# library(mapdata)
-library(mapproj)
 library(AmesHousing)
 
-source("helpers.R")
+ames <- make_ames()
 
-# iowa <- map("state", "iowa")
-counties <- readRDS("data/counties.rds")
-# ames <- make_ames()
-
-# Define UI for application
+# Define UI for application 
 ui <- page_sidebar(
-  title = "censusVis",
-  sidebar = sidebar(
-    helpText(
-      "Create demographic maps with information from the 2010 US Census."
-    ),
+  sidebar=sidebar(
     selectInput(
-      "var",
-      label = "Choose a variable to display",
-      choices =
-        c(
-          "Percent White",
-          "Percent Black",
-          "Percent Hispanic",
-          "Percent Asian"
-        ),
-      selected = "Percent White"
-    ),
-    sliderInput(
-      "range",
-      label = "Range of interest:",
-      min = 0,
-      max = 100,
-      value = c(0, 100)
-    ),
-  ),card(plotOutput("map"))
+      inputId="neighborhood",
+      label="Neighborhood",
+      choices=c(
+        "North Ames"="NAmes",
+        "Edwards"="edwards",
+        "Brookside"="Brkside"
+      ),
+      selected="Names"
+    )
+  ),
+  card(
+    plotOutput(outputId = "scatterplot")
+  )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic required to render output
 server <- function(input, output) {
-  output$map <- renderPlot({
-    data <- switch(input$var,
-                   "Percent White" = counties$white,
-                   "Percent Black" = counties$black,
-                   "Percent Hispanic" = counties$hispanic,
-                   "Percent Asian" = counties$asian)
-    
-    color <- switch(input$var,
-                    "Percent White" = "darkgreen",
-                    "Percent Black" = "black",
-                    "Percent Hispanic" = "darkorange",
-                    "Percent Asian" = "darkviolet")
-    
-    legend <- switch(input$var,
-                     "Percent White" = "% White",
-                     "Percent Black" = "% Black",
-                     "Percent Hispanic" = "% Hispanic",
-                     "Percent Asian" = "% Asian")
-    
-    percent_map(data, color, legend, input$range[1], input$range[2])
+  output$scatterplot <- renderPlot({
+    ggplot(data=ames, aes_string(x=input$sqft, y=input$price)) +
+      geom_point(method="lm")
   })
 }
 
